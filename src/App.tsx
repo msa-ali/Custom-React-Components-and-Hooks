21f105efa26b588677c4223f43e6a9df3ab09c57
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-12-13 20:41:23 +0530

    dropdown changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,24 @@
-import Accordion from "./components/Accordion";
-import ButtonPage from "./pages/ButtonPage";
+import { useState } from "react";
+import Dropdown, { DropdownItem } from "./components/Dropdown";
+
 
 function App() {
+  const [selection, setSelection] = useState<DropdownItem | null>(null);
 
-  const items = [
-    {
-      id: 1,
-      label: 'Can I use React on a project',
-      content: 'React is Awesome. You can use React on any project you want.',
-    },
-    {
-      id: 2,
-      label: 'Can I use Javascript on a project',
-      content: 'Yes, Javascript is Awesome. You can use Javascript on any project you want.',
-    },
-    {
-      id: 3,
-      label: 'Can I use CSS on a project',
-      content: <span>Yes, <b>CSS</b> is Awesome. You can use CSS on any project you want.</span>,
-    }
+  const handleSelection = (option: DropdownItem) => setSelection(option);
+
+  const options = [
+    { label: 'Red', value: 'red' },
+    { label: 'Green', value: 'green' },
+    { label: 'Blue', value: 'blue' },
   ];
 
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <ButtonPage />
-      <div className=" w-3/5 mb-8">
-      <Accordion items={items} />
+    <div className="flex flex-col justify-center items-center mt-8">
+      <div className="flex justify-between">
+        <Dropdown options={options} value={selection} onChange={handleSelection} />
+        <Dropdown options={options} value={selection} onChange={handleSelection} />
       </div>
     </div>
   );
---
+++ src/pages/AccordionPage.tsx
@@ -0,0 +1,33 @@
+import Accordion from "../components/Accordion";
+
+function AccordionPage() {
+
+    const items = [
+        {
+            id: 1,
+            label: 'Can I use React on a project',
+            content: 'React is Awesome. You can use React on any project you want.',
+        },
+        {
+            id: 2,
+            label: 'Can I use Javascript on a project',
+            content: 'Yes, Javascript is Awesome. You can use Javascript on any project you want.',
+        },
+        {
+            id: 3,
+            label: 'Can I use CSS on a project',
+            content: <span>Yes, <b>CSS</b> is Awesome. You can use CSS on any project you want.</span>,
+        }
+    ];
+
+
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <div className=" w-3/5 mb-8">
+                <Accordion items={items} />
+            </div>
+        </div>
+    );
+}
+
+export default AccordionPage;
