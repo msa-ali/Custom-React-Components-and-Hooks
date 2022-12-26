cad75a611d863801a0b0afec7621230d14a13492
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2022-12-26 17:57:16 +0530

    modal component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -4,6 +4,7 @@ const links: { label: string, path: string }[] = [
     { label: 'Dropdown', path: '/' },
     { label: 'Accordion', path: '/accordion' },
     { label: 'Buttons', path: '/buttons' },
+    { label: 'Modal', path: '/modal' },
 
 ];
 
